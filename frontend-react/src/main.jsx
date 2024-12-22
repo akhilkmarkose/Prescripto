4e9459f2be8664f9ac3b4a6065974d0cf7219f35
akhilkmarkose <akhilkmarkose@gmail.com>
2024-12-22 20:19:54 +0530

    Delete - StrictMode

--- frontend-react/src/main.jsx
+++ frontend-react/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -6,11 +5,9 @@ import { BrowserRouter } from "react-router-dom";
 import AppContextProvider from "./context/AppContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
       <AppContextProvider>
         <App />
       </AppContextProvider>
     </BrowserRouter>
-  </StrictMode>
 );
