dcb92ca080d79df5054bf67df407e6f605aa2709
Akhil K Markose <akhil.km@pitsolutions.com>
2025-01-04 16:06:09 +0530

    Update - Code formatting for server.js

--- backend-node/server.js
+++ backend-node/server.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
 
 // app config
 const app = express();
@@ -11,10 +11,10 @@ app.use(express.json());
 app.use(cors());
 
 // api endpoints
-app.get('/', (req, res) => {
-    res.status(200).send('Hello World');
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World");
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
