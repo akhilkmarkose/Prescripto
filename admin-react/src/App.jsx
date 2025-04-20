49348cc92cae84ac1e250700a0377205cc8dc24e
Akhil K Markose <akhil.km@pitsolutions.com>
2025-04-20 12:41:36 +0530

    Add - Component Navbar

--- admin-react/src/App.jsx
+++ admin-react/src/App.jsx
@@ -2,13 +2,15 @@ import React, { useContext } from "react";
 import Login from "./pages/login";
 import { ToastContainer } from "react-toastify";
 import { AdminContext } from "./context/AdminContext";
+import Navbar from './components/Navbar';
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
 
   return aToken ? (
-    <div>
+    <div className="bg-[#F8F9FD]">
       <ToastContainer />
+      <Navbar />
     </div>
   ) : (
     <>
