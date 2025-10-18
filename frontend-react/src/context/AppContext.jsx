885a32e00f7c1aff8b32021723b3ea85706ea41a
Akhil K Markose <akhilkmarkose@gmail.com>
2025-10-18 17:53:38 +0530

    Add: Get doctor list

--- frontend-react/src/context/AppContext.jsx
+++ frontend-react/src/context/AppContext.jsx
@@ -1,21 +1,44 @@
-import { createContext } from 'react';
-import { doctors } from '../assets/assets';
+import { createContext, useState, useEffect } from "react";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 export const AppContext = createContext();
 
-const currencySymbol = '$';
+const currencySymbol = "$";
 
 const AppContextProvider = (props) => {
-    const value = {
-        doctors,
-        currencySymbol
-    };
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
-    return (
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-    )
-}
+  const [doctors, setDoctors] = useState([]);
+
+  // Getting Doctors using API
+  const getDoctorsData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/doctor/list");
+      if (data.success) {
+        setDoctors(data.doctors);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getDoctorsData();
+  }, []);
+
+  const value = {
+    doctors,
+    getDoctorsData,
+    currencySymbol
+  };
+
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
 
 export default AppContextProvider;
