1f72cf30e3fbb6aa4ad2d8e2d1a6ae65bcd7f146
Akhil K Markose <akhilkmarkose@gmail.com>
2025-10-20 18:15:07 +0530

    Add: API endpoint to fetch admin dashboard data

--- backend-node/controllers/adminController.js
+++ backend-node/controllers/adminController.js
@@ -3,6 +3,7 @@ import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
 import doctorModel from "../models/doctorModel.js";
 import appointmentModel from "../models/appointmentModel.js";
+import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 
 const addDoctor = async (req, res) => {
@@ -145,10 +146,32 @@ const appointmentCancel = async (req, res) => {
   }
 };
 
+// API to get dashboard data for admin panel
+const adminDashboard = async (req, res) => {
+  try {
+    const doctors = await doctorModel.find({});
+    const users = await userModel.find({});
+    const appointments = await appointmentModel.find({});
+
+    const dashData = {
+      doctors: doctors.length,
+      appointments: appointments.length,
+      patients: users.length,
+      latestAppointments: appointments.reverse(),
+    };
+
+    res.json({ success: true, dashData });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
 export {
   addDoctor,
   loginAdmin,
   allDoctors,
   appointmentsAdmin,
   appointmentCancel,
+  adminDashboard,
 };
