4e2ab0f5e26e616d862f7edda9056d5f598ca4bd
Akhil K Markose <akhilkmarkose@gmail.com>
2025-10-20 17:21:10 +0530

    Update: Appointment listing with age and date formatting

--- admin-react/src/context/AppContext.jsx
+++ admin-react/src/context/AppContext.jsx
@@ -1,15 +1,49 @@
-import { createContext } from 'react';
+import { createContext } from "react";
 
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
-    const value = {};
+  const currency = "$";
+  const calculateAge = (dob) => {
+    const today = new Date();
+    const birthDate = new Date(dob);
 
+    let age = today.getFullYear() - birthDate.getFullYear();
+    return age;
+  };
+
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  // Function to format the date eg. ( 20_01_2000 => 20 Jan 2000 )
+  const slotDateFormat = (slotDate) => {
+    const dateArray = slotDate.split("_");
     return (
-        <AppContext.Provider value={value}>
-            { props.children }
-        </AppContext.Provider>
-    )
-}
+      dateArray[0] + " " + months[Number(dateArray[1]) - 1] + " " + dateArray[2]
+    );
+  };
 
-export default AppContextProvider
+  const value = {
+    calculateAge,
+    slotDateFormat,
+    currency,
+  };
+
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
+
+export default AppContextProvider;
