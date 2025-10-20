e04f370f40f48a2e4c247a947b4f9a8bdac4c654
Akhil K Markose <akhilkmarkose@gmail.com>
2025-10-21 00:57:08 +0530

    Add: API integration and UI implementation for doctor appointments

--- admin-react/src/context/DoctorContext.jsx
+++ admin-react/src/context/DoctorContext.jsx
@@ -1,4 +1,6 @@
 import { createContext, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 export const DoctorContext = createContext();
 
@@ -8,10 +10,33 @@ const DoctorContextProvider = (props) => {
   const [dToken, setDToken] = useState(
     localStorage.getItem("dToken") ? localStorage.getItem("dToken") : ""
   );
+  const [appointments, setAppointments] = useState([]);
+
+  // Getting Doctor appointment data from Database using API
+  const getAppointments = async () => {
+    try {
+      const { data } = await axios.get(
+        backendUrl + "/api/doctor/appointments",
+        { headers: { dToken } }
+      );
+
+      if (data.success) {
+        setAppointments(data.appointments.reverse());
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
   const value = {
     dToken,
     setDToken,
     backendUrl,
+    appointments,
+    getAppointments,
   };
 
   return (
