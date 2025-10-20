720bdbce36cb1c30d1c7b97cd472bd0609bb27c0
Akhil K Markose <akhilkmarkose@gmail.com>
2025-10-20 14:33:35 +0530

    Add: API endpoint to fetch all appointments fro admin

--- backend-node/controllers/adminController.js
+++ backend-node/controllers/adminController.js
@@ -1,8 +1,9 @@
 import validator from "validator";
 import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
-import doctorModel from '../models/doctorModel.js';
-import jwt from 'jsonwebtoken';
+import doctorModel from "../models/doctorModel.js";
+import appointmentModel from "../models/appointmentModel.js";
+import jwt from "jsonwebtoken";
 
 const addDoctor = async (req, res) => {
   try {
@@ -89,31 +90,44 @@ const addDoctor = async (req, res) => {
 
 // API for admin login
 const loginAdmin = async (req, res) => {
-    try{
-        const { email, password } = req.body;
-
-        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
-            const token = jwt.sign(email+password, process.env.JWT_SECRET);
-            res.json({ success: true, token });
-        } else {
-            res.json({ success: false, message: "Invalid credentials" });
-        }
-    } catch (error) {
-        console.log(error);
-        res.json({ success: false, message: error.message });
-    }
-}
-
-// API to get all doctors list for admin panel
-const allDoctors = async (req, res) => {
   try {
-    const doctors = await doctorModel.find({}).select('-password');
-    res.json({ success: true, doctors });
-    
+    const { email, password } = req.body;
+
+    if (
+      email === process.env.ADMIN_EMAIL &&
+      password === process.env.ADMIN_PASSWORD
+    ) {
+      const token = jwt.sign(email + password, process.env.JWT_SECRET);
+      res.json({ success: true, token });
+    } else {
+      res.json({ success: false, message: "Invalid credentials" });
+    }
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
   }
-}
+};
 
-export { addDoctor, loginAdmin, allDoctors };
+// API to get all doctors list for admin panel
+const allDoctors = async (req, res) => {
+  try {
+    const doctors = await doctorModel.find({}).select("-password");
+    res.json({ success: true, doctors });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// API to get all appointments list
+const appointmentsAdmin = async (req, res) => {
+  try {
+    const appointments = await appointmentModel.find({});
+    res.json({ success: true, appointments });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { addDoctor, loginAdmin, allDoctors, appointmentsAdmin };
